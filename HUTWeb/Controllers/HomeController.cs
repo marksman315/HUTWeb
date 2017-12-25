765cc809db2f8a1c9fb1679cecb87a5f4fbb39c7
Michael Marks <mmarks315@charter.net>
2017-12-25 15:56:37 -0600

    ability to add weight

--- HUTWeb/Controllers/HomeController.cs
+++ HUTWeb/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HUTModels;
 using HUTWeb.Models;
 
 namespace HUTWeb.Controllers
@@ -12,8 +13,28 @@ namespace HUTWeb.Controllers
         public ActionResult Index()
         {
             PersonHandler handler = new PersonHandler();
-           
-            return View(handler.GetAll());
+            HomeModel model = new HomeModel();
+
+            List<Person> list = handler.GetAll();
+            List<SelectListItem> items = new List<SelectListItem>();
+            
+            foreach (Person p in list)
+            {
+                items.Add(new SelectListItem() { Text = p.Firstname + " " + p.Lastname, Value = p.PersonId.ToString() });
+            }
+
+            model.Persons = items;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult RecordWeight(string personId, string weightValue)
+        {           
+            WeightHandler handler = new WeightHandler();
+            handler.Insert(Convert.ToInt32(personId), Convert.ToDecimal(weightValue), DateTime.Now);
+
+            return Json("Posted Good");
         }
 
         public ActionResult About()
---
+++ HUTWeb/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HUTModels;
+
+namespace HUTWeb.Models
+{
+    public class HomeModel
+    {
+        public List<SelectListItem> Persons { get; set; }
+    }
+}
