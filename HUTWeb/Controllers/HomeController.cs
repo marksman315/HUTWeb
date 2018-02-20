283b226c28fef3edf423618d80dbc730ef695a13
Michael Marks <mmarks315@charter.net>
2018-02-19 21:21:29 -0600

    moved time, added food and recipe controllers

--- HUTWeb/Controllers/HomeController.cs
+++ HUTWeb/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using HUTModels;
 using HUTWeb.Models;
-using Newtonsoft.Json;
 
 namespace HUTWeb.Controllers
 {
