f7e63b84849d2fa4f7c2a206679edca2dd38be78
Michael Marks <mmarks315@charter.net>
2018-03-01 18:24:31 -0600

    added hack for keep alive functionality (not recommended for production)

---
+++ HUTWeb/Controllers/KeepAliveController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using HUTWeb.Handlers;
+
+namespace HUTWeb.Controllers
+{
+    public class KeepAliveController : Controller
+    {
+        //GET: KeepAlive
+        [HttpGet]
+        public JsonResult KeepAlive()
+        {
+            KeepAliveHandler handler = new KeepAliveHandler();
+            string text = handler.GetKeepAlive();
+
+            return Json(text, JsonRequestBehavior.AllowGet);
+        }        
+    }
+}
---
+++ HUTWeb/Handlers/KeepAliveHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HUTWeb.Helpers;
+using Newtonsoft.Json;
+
+namespace HUTWeb.Handlers
+{
+    public class KeepAliveHandler : BaseHandler
+    {        
+        public KeepAliveHandler() : base()
+        {
+        }
+      
+        public string GetKeepAlive()
+        {
+            Uri uri = new Uri(this.BaseWebAPIURL + "KeepAlive");
+
+            var result = HttpHelper.GetValues(uri).Result;
+
+            if (result != null)
+            {
+                return JsonConvert.DeserializeObject<string>(result);
+            }
+
+            return null;
+        }
+    }
+}
